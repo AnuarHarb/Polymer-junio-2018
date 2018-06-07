e7fe04b8144ec07dd0b11a1cd40c205f029ead66
Osvaldo Alcantara <alcantaraosvaldo@gmail.com>
2018-06-06 22:07:50 -0500

    refactor code

--- osvaldo/es6/index.js
+++ osvaldo/es6/index.js
@@ -1,46 +1,67 @@
 
-(function(){
+(function () {
     let loaderBar = document.querySelector(".loader");
     let button = document.querySelector("button");
-    
-
-    button.addEventListener("click", runLoader);
+    button.addEventListener("click", startLoader);
 
     let percentage = 0;
-
-    
-
 
     function setRandomInterval() {
         return Math.floor((Math.random() * 10) + 5);
     }
-    
-    function getARandomInterval() {
-        return Math.floor((Math.random() * 5) + 1);
+
+    function startLoader() {
+        alert("hi")
+        while (percentage < 120) {
+            myFunc()
+
+        }
+
     }
-    
-     function runLoader() {
-         
-         setInterval(function(){
-             if(percentage < 120) {
-                 loaderBar.style.setProperty('--progress', `${percentage}%`);
-                 percentage += setRandomInterval();
-                 if(percentage > 120) {
-                    document.querySelector(".dog").style.display = "block";
-                }
-             }
 
-             
-            console.log("boom")
-        }, getARandomInterval()*100);
-        
-     }
+    function myFunc() {
+        setInterval(function () {
+            aconsole.log(percentage)
+            percentage += 20
+        }, 3000);
+    }
 
-    
-    
-    
+    function getNumBetween2And6() {
+        let num = Math.floor((Math.random() * 5) + 1);
+        if (isOddNumer(num)) { num++ };
+        console.log(num);
+        return num;
+    }
 
-    
+    function runLoader() {
+        if (percentage < 120) {
+            loaderBar.style.setProperty('--progress', `${percentage}%`);
+            percentage += setRandomInterval();
+            if (percentage > 120) {
+                document.querySelector(".dog").style.display = "block";
+            }
+        }
+    }
+
+    function incrementLoader(num) {
+        loaderBar.style.setProperty('--progress', `${percentage}%`);
+    }
+
+    function incrementPercentage(num) {
+        percentage = percentage + num;
+    }
+
+    function isOddNumer(num) {
+        if (num % 2 !== 0) {
+            return true
+        }
+    }
+
+
+
+
+
+
 })()
 
 
