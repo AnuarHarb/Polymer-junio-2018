34cd22346514a9af850527815ec071dd4244ef31
Mario Barceinas <mario@MacBook-Pro-4.local>
2018-06-08 09:15:01 -0500

    Loader with 100% VanillaJS

--- devbarceinas/es6/loader/index.js
+++ devbarceinas/es6/loader/index.js
@@ -2,34 +2,35 @@
 
     'use strict';
 
-    const loader__action    = document.getElementById('#loader__action');
+    const loader__action    = document.getElementById('loader__action');
     const loader__bar       = document.getElementsByClassName('loader__bar')[0];
 
-    document.getElementById('loader__action').addEventListener('click', () => {
-        loading(0);
+    loader__action.addEventListener('click', () => {
+        loading(0); 
     });
 
     const loading = (anch) => {
         if (anch >= 100) {
-            console.log('Datos cargados!!');
+            document.getElementById("loader__message").innerHTML = "Welcome Barrio !! :D";
         } else {
-            let random = 100;
-            let action = anch + Math.round(Math.random() * (random - anch));
+            let random = Math.round(Math.random() * (100 - anch));
+            let action = anch + random
+             ;
             loadingVelocy(action);
         }
     }
 
     const loadingVelocy = (anch) => {
         let time = loadingSlow();
-        loader__bar.style.duration = time + 's';
-        loader__bar.style.width = anch + '%';
+        loader__bar.style.duration = `${time}s`;
+        loader__bar.style.width = `${anch}%`;
         setTimeout(() => {
           loading(anch);
         }, time * 1000);
       }
 
     const loadingSlow = () => {
-        let velocy = Math.ceil(Math.random() * 10);
+        let velocy = Math.ceil(Math.random() * 5);
         return velocy;
     }
 
