6e855e4a80dbcd1f765b8653ad95595578b8ace4
Alan Valery Minor Leyva <AlanMinor@Alan-Minor.local>
2018-06-08 12:23:28 -0500

    Practice Tabs Changes

--- Alan/Es6/Tabs/index.js
+++ Alan/Es6/Tabs/index.js
@@ -2,6 +2,7 @@ const tab_title = document.querySelector('.tab_title');
 const button = document.querySelector('.new_tab');
 const input = document.getElementById('title');
 const tabs = document.querySelector('.tab');
+const buttonNewTab = document.querySelector('#createTab');
 
 var conTabs = 1;
 var tabList = [];
@@ -13,17 +14,20 @@ var titleList= [];
     button.addEventListener('click', () => {
 
         var newtab = `<button class="tablinks" id="tab${conTabs}" onclick="viewTitle(${conTabs}-1)">Tab${conTabs}</button>`; 
-        console.log(newtab);
         tabList.push(newtab);
         tabs.innerHTML = tabList;
 
         var newTitle = new Tab(input.value);
         input.value = "";
-
         titleList.push(newTitle);
-        console.log(titleList[0]);
 
         conTabs = conTabs + 1;
+    });
+
+    buttonNewTab.addEventListener('click', () => {
+        button.style.setProperty('display', 'inline-block');
+        input.style.setProperty('display', 'inline-block');
+        document.querySelector('.title_tab').style.setProperty('display', 'none');
     });
 
     class Tab{
@@ -33,6 +37,8 @@ var titleList= [];
     
         mostrarTab(){
             tab_title.innerHTML = `<h2 class="title_tab">${this.title}</h2>`;
+            button.style.setProperty('display', 'none');
+            input.style.setProperty('display', 'none');
         } 
     }
 
